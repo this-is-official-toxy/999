62b20abf1a3d044fbdb65e0e0b8008cd7b60249b
TOXIC DEVIL <toxicdevil.abhinav@gmail.com>
2021-11-11 11:28:28 +0530

    New Base 🥳

--- alexa/Base.js
+++ alexa/Base.js
@@ -1,15 +1,70 @@
-'use strict';
-
-class Base {
-    constructor(client) {
-        Object.defineProperty(this, 'client', { value: client });
+"use strict";
+var _0x5b18aa = _0x25da;
+(function (_0x272f72, _0xb00677) {
+  var _0x9c1209 = _0x25da,
+    _0x428fdc = _0x272f72();
+  while (!![]) {
+    try {
+      var _0x2d84c2 =
+        (-parseInt(_0x9c1209(0x11b)) / 0x1) *
+          (-parseInt(_0x9c1209(0x125)) / 0x2) +
+        -parseInt(_0x9c1209(0x11f)) / 0x3 +
+        -parseInt(_0x9c1209(0x124)) / 0x4 +
+        (parseInt(_0x9c1209(0x121)) / 0x5) *
+          (parseInt(_0x9c1209(0x118)) / 0x6) +
+        -parseInt(_0x9c1209(0x119)) / 0x7 +
+        -parseInt(_0x9c1209(0x11e)) / 0x8 +
+        (parseInt(_0x9c1209(0x11a)) / 0x9) * (parseInt(_0x9c1209(0x120)) / 0xa);
+      if (_0x2d84c2 === _0xb00677) break;
+      else _0x428fdc["push"](_0x428fdc["shift"]());
+    } catch (_0x534283) {
+      _0x428fdc["push"](_0x428fdc["shift"]());
     }
-
-    _clone() {
-        return Object.assign(Object.create(this), this);
-    }
-    
-    _patch(data) { return data; }
+  }
+})(_0x3878, 0xa4868);
+function _0x25da(_0x8b7c3b, _0x410f52) {
+  var _0x3878bd = _0x3878();
+  return (
+    (_0x25da = function (_0x25da89, _0x29eccb) {
+      _0x25da89 = _0x25da89 - 0x118;
+      var _0x2a5b36 = _0x3878bd[_0x25da89];
+      return _0x2a5b36;
+    }),
+    _0x25da(_0x8b7c3b, _0x410f52)
+  );
 }
-
-module.exports = Base;
+function _0x3878() {
+  var _0x30bf20 = [
+    "_clone",
+    "3104992zdUIrn",
+    "1183626PzQiCP",
+    "340ekONSe",
+    "5eVUaZb",
+    "_patch",
+    "exports",
+    "89760OThiYX",
+    "119892HrYPCw",
+    "7436640uQvvKI",
+    "2973306armppr",
+    "64773NgnxMJ",
+    "7NyqIFY",
+    "assign",
+  ];
+  _0x3878 = function () {
+    return _0x30bf20;
+  };
+  return _0x3878();
+}
+class Base {
+  constructor(_0x6f2cdb) {
+    Object["defineProperty"](this, "client", { value: _0x6f2cdb });
+  }
+  [_0x5b18aa(0x11d)]() {
+    var _0x462870 = _0x5b18aa;
+    return Object[_0x462870(0x11c)](Object["create"](this), this);
+  }
+  [_0x5b18aa(0x122)](_0x43e6e2) {
+    return _0x43e6e2;
+  }
+}
+module[_0x5b18aa(0x123)] = Base;
