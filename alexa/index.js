99e62f3964792cb51ae420cea6d41aa03ce3a7a0
TOXIC DEVIL <toxicdevil.abhinav@gmail.com>
2021-11-11 11:33:44 +0530

    New Base 🥳

--- alexa/index.js
+++ alexa/index.js
@@ -1,8 +1,63 @@
-module.exports = {
-    Base: require('./Base'),
-    Message: require('./Message'),
-    StringSession: require('./StringSession'),
-    ReplyMessage: require('./ReplyMessage'),
-    Image: require('./Image'),
-    Video: require('./Video')
-};
+function _0x4cf0() {
+  var _0x49a434 = [
+    "./Base",
+    "4255424MdpurV",
+    "36866gmsVsl",
+    "970FSHfLN",
+    "./Message",
+    "1097538dWcDrK",
+    "./ReplyMessage",
+    "5821134serVwV",
+    "./StringSession",
+    "10936NqUGIw",
+    "9328592pFRSYX",
+    "exports",
+    "3781914fNoqdQ",
+    "./Image",
+  ];
+  _0x4cf0 = function () {
+    return _0x49a434;
+  };
+  return _0x4cf0();
+}
+function _0x117f(_0x3ff053, _0x27b762) {
+  var _0x4cf0af = _0x4cf0();
+  return (
+    (_0x117f = function (_0x117f11, _0x424352) {
+      _0x117f11 = _0x117f11 - 0x1a2;
+      var _0x3cd23f = _0x4cf0af[_0x117f11];
+      return _0x3cd23f;
+    }),
+    _0x117f(_0x3ff053, _0x27b762)
+  );
+}
+var _0x1860d0 = _0x117f;
+(function (_0x263103, _0x1f36bb) {
+  var _0x69ae8b = _0x117f,
+    _0x4068e9 = _0x263103();
+  while (!![]) {
+    try {
+      var _0x4ae741 =
+        parseInt(_0x69ae8b(0x1a9)) / 0x1 +
+        parseInt(_0x69ae8b(0x1a6)) / 0x2 +
+        parseInt(_0x69ae8b(0x1a2)) / 0x3 +
+        (-parseInt(_0x69ae8b(0x1ad)) / 0x4) *
+          (parseInt(_0x69ae8b(0x1a7)) / 0x5) +
+        parseInt(_0x69ae8b(0x1ab)) / 0x6 +
+        -parseInt(_0x69ae8b(0x1ae)) / 0x7 +
+        -parseInt(_0x69ae8b(0x1a5)) / 0x8;
+      if (_0x4ae741 === _0x1f36bb) break;
+      else _0x4068e9["push"](_0x4068e9["shift"]());
+    } catch (_0x306ddc) {
+      _0x4068e9["push"](_0x4068e9["shift"]());
+    }
+  }
+})(_0x4cf0, 0xe860a),
+  (module[_0x1860d0(0x1af)] = {
+    Base: require(_0x1860d0(0x1a4)),
+    Message: require(_0x1860d0(0x1a8)),
+    StringSession: require(_0x1860d0(0x1ac)),
+    ReplyMessage: require(_0x1860d0(0x1aa)),
+    Image: require(_0x1860d0(0x1a3)),
+    Video: require("./Video"),
+  });
