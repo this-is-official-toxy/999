760c9e4cbe8427d990fdf565168d17285ae694af
TOXIC DEVIL <toxicdevil.abhinav@gmail.com>
2021-11-22 12:34:49 +0530

    New Async Function 🥳

--- plugins/heroku.js
+++ plugins/heroku.js
@@ -17,10 +17,10 @@ let baseURI = '/apps/' + Config.HEROKU.APP_NAME;
 
 WhatsAlexa.addCommand({pattern: 'restart', fromMe: true, desc: Lang.RESTART_DESC}, (async (message, match) => {
 
-    await message.client.sendMessage(message.jid,Lang.RESTART_MSG, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: message.data})
+    await message.sendReply(Lang.RESTART_MSG);
     console.log(baseURI);
     await heroku.delete(baseURI + '/dynos').catch(async (error) => {
-        await message.client.sendMessage(message.jid,error.message, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: message.data})
+        await message.sendReply(error.message);
     });
 }));
 
@@ -28,20 +28,20 @@ WhatsAlexa.addCommand({pattern: 'shutdown', fromMe: true, desc: Lang.SHUTDOWN_DE
 
     await heroku.get(baseURI + '/formation').then(async (formation) => {
         forID = formation[0].id;
-        await message.client.sendMessage(message.jid,Lang.SHUTDOWN_MSG, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: message.data})
+        await message.sendReply(Lang.SHUTDOWN_MSG);
         await heroku.patch(baseURI + '/formation/' + forID, {
             body: {
                 quantity: 0
             }
         });
     }).catch(async (err) => {
-        await message.client.sendMessage(message.jid,error.message, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: message.data})
+        await message.sendReply(error.message);
     });
 }));
 
 WhatsAlexa.addCommand({pattern: 'editvar ?(.*)', fromMe: true, desc: Lang.SETVAR_DESC}, (async(message, match) => {
 
-    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.KEY_VAL_MISSING, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: message.data})
+    if (match[1] === '') return await message.sendReply(Lang.KEY_VAL_MISSING);
 
     if ((varKey = match[1].split(':')[0]) && (varValue = match[1].split(':')[1])) {
         await heroku.patch(baseURI + '/config-vars', {
@@ -49,16 +49,16 @@ WhatsAlexa.addCommand({pattern: 'editvar ?(.*)', fromMe: true, desc: Lang.SETVAR
                 [varKey]: varValue
             }
         }).then(async (app) => {
-            await message.client.sendMessage(message.jid,Lang.SET_SUCCESS.format(varKey, varValue), MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: message.data})
+            await message.sendReply(Lang.SET_SUCCESS.format(varKey, varValue));
         });
     } else {
-        await message.client.sendMessage(message.jid,Lang.INVALID, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: message.data})
+        await message.sendReply(Lang.INVALID);
     }
 }));
 
 WhatsAlexa.addCommand({pattern: 'removevar ?(.*)', fromMe: true, desc: Lang.DELVAR_DESC}, (async (message, match) => {
 
-    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.KEY_VAL_MISSING, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: message.data})
+    if (match[1] === '') return await message.sendReply(Lang.KEY_VAL_MISSING);
     await heroku.get(baseURI + '/config-vars').then(async (vars) => {
         key = match[1].trim();
         for (vr in vars) {
@@ -68,26 +68,26 @@ WhatsAlexa.addCommand({pattern: 'removevar ?(.*)', fromMe: true, desc: Lang.DELV
                         [key]: null
                     }
                 });
-                return await message.client.sendMessage(message.jid,Lang.DEL_SUCCESS.format(key), MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: message.data})
+                return await message.sendReply(Lang.DEL_SUCCESS.format(key));
             }
         }
-        await message.client.sendMessage(message.jid,Lang.NOT_FOUND, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: message.data})
+        await message.sendReply(Lang.NOT_FOUND);
     }).catch(async (error) => {
-        await message.client.sendMessage(message.jid,error.message, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: message.data})
+        await message.sendReply(error.message);
     });
 
 }));
 
 WhatsAlexa.addCommand({pattern: 'getvar ?(.*)', fromMe: true, desc: Lang.GETVAR_DESC}, (async (message, match) => {
 
-    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.KEY_VAL_MISSING, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: message.data})
+    if (match[1] === '') return await message.sendReply(Lang.KEY_VAL_MISSING);
     await heroku.get(baseURI + '/config-vars').then(async (vars) => {
         for (vr in vars) {
-            if (match[1].trim() == vr) return await message.sendMessage("```{} - {}```".format(vr, vars[vr]));
+            if (match[1].trim() == vr) return await message.sendReply("```{} - {}```".format(vr, vars[vr]));
         }
-        await message.client.sendMessage(message.jid,Lang.NOT_FOUND, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: message.data})
+        await message.sendReply(Lang.NOT_FOUND);
     }).catch(async (error) => {
-        await message.client.sendMessage(message.jid,error.message, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: message.data})
+        await message.sendReply(error.message);
     });
 }));
 
@@ -96,8 +96,6 @@ if (Config.WORKTYPE == 'private') {
     WhatsAlexa.addCommand({pattern: 'dyno', fromMe: true, desc: Lang.DYNO_DESC}, (async (message, match) => {
 
         heroku.get('/account').then(async (account) => {
-            // have encountered some issues while calling this API via heroku-client
-            // so let's do it manually
             url = "https://api.heroku.com/accounts/" + account.id + "/actions/get-quota"
             headers = {
                 "User-Agent": "Chrome/80.0.3987.149 Mobile Safari/537.36",
@@ -110,16 +108,16 @@ if (Config.WORKTYPE == 'private') {
                quota_used = Math.floor(resp.quota_used);         
                percentage = Math.round((quota_used / total_quota) * 100);
                remaining = total_quota - quota_used;
-               await message.client.sendMessage(
+               await message.sendReply(
                     message.jid,
                     Lang.DYNO_TOTAL + ": ```{}```\n\n".format(secondsToHms(total_quota))  + 
                     Lang.DYNO_USED + ": ```{}```\n".format(secondsToHms(quota_used)) +  
                     Lang.PERCENTAGE + ": ```{}```\n\n".format(percentage) +
                     Lang.DYNO_LEFT + ": ```{}```\n".format(secondsToHms(remaining)),
-                    MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: message.data
+                    );
                })
             }).catch(async (err) => {
-                await message.client.sendMessage(message.jid,err.message, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: message.data})
+                await message.sendReply(err.message);
             });        
         });
     }));
@@ -129,8 +127,6 @@ else if (Config.WORKTYPE == 'public') {
     WhatsAlexa.addCommand({pattern: 'dyno', fromMe: false, desc: Lang.DYNO_DESC}, (async (message, match) => {
 
         heroku.get('/account').then(async (account) => {
-            // have encountered some issues while calling this API via heroku-client
-            // so let's do it manually
             url = "https://api.heroku.com/accounts/" + account.id + "/actions/get-quota"
             headers = {
                 "User-Agent": "Chrome/80.0.3987.149 Mobile Safari/537.36",
@@ -143,16 +139,16 @@ else if (Config.WORKTYPE == 'public') {
                quota_used = Math.floor(resp.quota_used);         
                percentage = Math.round((quota_used / total_quota) * 100);
                remaining = total_quota - quota_used;
-               await message.client.sendMessage(
+               await message.sendReply(
                     message.jid,
                     Lang.DYNO_TOTAL + ": ```{}```\n\n".format(secondsToHms(total_quota))  + 
                     Lang.DYNO_USED + ": ```{}```\n".format(secondsToHms(quota_used)) +  
                     Lang.PERCENTAGE + ": ```{}```\n\n".format(percentage) +
                     Lang.DYNO_LEFT + ": ```{}```\n".format(secondsToHms(remaining)),
-                    MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: message.data
+                    );
                })
             }).catch(async (err) => {
-                await message.client.sendMessage(message.jid,err.message, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: message.data})
+                await message.sendReply(err.message);
             });        
         });
     }));
