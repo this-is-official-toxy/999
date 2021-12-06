59d2677529035d95411c4fe631572c5e6f8ed88d
TOXIC DEVIL <toxicdevil.abhinav@gmail.com>
2021-12-06 11:24:22 +0530

    Added Index 🥳

---
+++ src/modules/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    getRandom: require('./getRandom'),
+    isUrl: require('./isUrl'),
+    sticker2Video: require('./sticker2Video'),
+    uploadImage: require('./uploadImage')
+};
